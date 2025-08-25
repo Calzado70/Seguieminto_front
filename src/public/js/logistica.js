20c8f0f2d311bc6772be6e932d290547147fb220
gian carlos <giancarlosgiraldof@gmail.com>
2025-08-25 16:26:10 -0500

    rutas 5

--- src/public/js/logistica.js
+++ src/public/js/logistica.js
@@ -3,26 +3,26 @@ let historialCompletoEnviado = [];
 let paginaActual = 1;
 const registrosPorPagina = 10;
 
-function verificarTokenAlCargar() {
-    const token = localStorage.getItem('token');
-    if (!token) {
-        window.location.href = '/';
-        return;
-    }
+// function verificarTokenAlCargar() {
+//     const token = localStorage.getItem('token');
+//     if (!token) {
+//         window.location.href = '/';
+//         return;
+//     }
 
-    try {
-        const payload = JSON.parse(atob(token.split('.')[1]));
-        const expiracion = payload.exp * 1000;
-        if (Date.now() >= expiracion) {
-            localStorage.removeItem('token');
-            window.location.href = '/';
-        }
-    } catch (error) {
-        console.error('Error al verificar el token:', error);
-        localStorage.removeItem('token');
-        window.location.href = '/';
-    }
-}
+//     try {
+//         const payload = JSON.parse(atob(token.split('.')[1]));
+//         const expiracion = payload.exp * 1000;
+//         if (Date.now() >= expiracion) {
+//             localStorage.removeItem('token');
+//             window.location.href = '/';
+//         }
+//     } catch (error) {
+//         console.error('Error al verificar el token:', error);
+//         localStorage.removeItem('token');
+//         window.location.href = '/';
+//     }
+// }
 
 function redirigir(selectId) {
     const selectElement = document.getElementById(selectId);
